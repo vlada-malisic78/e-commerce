f4cee01a10cdffb92e5db2cfff4f7dd961d730d5
vlada-malisic78 <vladamalisic@yahoo.com>
2020-01-23 16:42:19 +0100

    Create poject with create reac app and added files for homepage

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import HomePage from './HomePage/HomePage';
+
+const App = () => {
+
+  return(
+    <div className="main">
+      <HomePage />
+    </div>
+  )
+}
+
+export default App;
